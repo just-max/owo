7202bc077594813533484d999efaa4e8fd312981
Max Lang <17551908+just-max@users.noreply.github.com>
2022-09-19 04:00:25 +0200

    to reduce unnecessary dependencies, inline un/bottom

--- owobot/cogs/bottom.py
+++ owobot/cogs/bottom.py
@@ -1,5 +1,9 @@
 from discord.ext import commands
-import bottom
+
+
+# finally, clean code
+encode,decode=lambda s:''.join(c//200*"🫂"+c%200//50*"💖"+c%50//10*"✨"+c%10//5*"🥺"+c%5*","+(c==0)*"❤️"+"👉👈"for c in s.encode()),lambda s:bytes([200*(c:=b.count)("🫂")+50*c("💖")+10*c("✨")+5*c("🥺")+c(",")for b in s.split("👉👈")[:-1]]).decode()
+# courtesy of bottom software foundation: https://github.com/bottom-software-foundation/oneline-bottom-py
 
 
 class Bottom(commands.Cog):
@@ -8,11 +12,11 @@ class Bottom(commands.Cog):
 
     @commands.hybrid_command(aliases=["bowottom"])
     async def bottom(self, ctx, *, msg: str):
-        await ctx.send(bottom.encode(msg))
+        await ctx.send(encode(msg))
 
     @commands.hybrid_command(aliases=["unbowottom"])
     async def unbottom(self, ctx, *, msg: str):
-        await ctx.send(bottom.decode(msg))
+        await ctx.send(decode(msg))
 
 
 def setup(bot):
